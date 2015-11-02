58da5ea18f157fd0eb2b84eac506829ca7225a55
Mark Plagge <plaggm@rpi.edu>
2015-11-02 14:17:39 -0500

    more cal testing + doc'

---
+++ PaxosDistributedCalendarMain/PaxosDistributedCalendarMain.py
@@ -0,0 +1,10 @@
+import Calendar, Interface, Log, Server
+
+## Main entry point for the software. This program will:
+# 1. Create a new Calendar.
+# 2. If a logfile exists, load it and add log entries to the generated calendar.
+# 3. Create a new server class
+# 4. Launch the interface controller, passing in the server class
+# 5. Handle clean exits :)
+
+## Some notes and lessions from previous work ##
