87e9640696af5a39ff2875cd50ef741deb672f8b
Mark Plagge <plaggm@rpi.edu>
2015-11-02 17:16:33 -0500

    added interface and pycharm files

--- Interface/Interface.py
+++ Interface/Interface.py
@@ -8,7 +8,7 @@ import weakref
 from urwid.raw_display import Screen
 
 
-loop = asyncio.get_event_loop()
+
 
 # ----- Widget Setup, from urwid example ----
 
@@ -25,11 +25,11 @@ def build_widgets():
         if not widget:
             #widget is dead; the main loop is destroyed
             return
-        
+
         widget.set_text(datetime.now().isoformat())
 
         loop.call_later(1,update_clock,widget_ref)
-    
+
         clock = urwid.Text('')
 
         update_clock(weakref.ref(clock))
@@ -42,19 +42,37 @@ def unhandled(key):
 
 
 def demo1():
-    main_widget = build_widgets()
+    txt = urwid.Text(u"Hello World!!!!")
+    fill = urwid.Filler(txt,'top')
     urwid_loop = urwid.MainLoop(
-        main_widget,
-        event_loop = urwid.AsyncioEventLoop(loop=loop),
+        fill,
+        event_loop = urwid.AsyncioEventLoop(loop=asyncio.get_event_loop()),
         unhandled_input=unhandled,
         )
     urwid_loop.run()
 
-demo1()
+def menu(title, choices):
+    body = [urwid.Text(title), urwid.Divider()]
+    for c in choices:
+        button = urwid.Button(c)
+        urwid.connect_signal(button, 'click', item_chosen, c)
+        body.append(urwid.AttrMap(button, None, focus_map='reversed'))
+    return urwid.ListBox(urwid.SimpleFocusListWalker(body))
+
+def item_chosen(button, choice):
+    response = urwid.Text([u'You chose ', choice, u'\n'])
+    done = urwid.Button(u'Ok')
+    urwid.connect_signal(done, 'click', exit_program)
+    main.original_widget = urwid.Filler(urwid.Pile([response,
+        urwid.AttrMap(done, None, focus_map='reversed')]))
+
+def exit_program(button):
+    raise urwid.ExitMainLoop()
 
 
 
 def mainMenu():
+
     pass
 
 def addCalendarEvent():
@@ -69,3 +87,13 @@ def viewLog():
 
 def startup():
     pass
+
+choices = u'Chapman Cleese Gilliam Idle Jones Palin'.split()
+
+main = urwid.Padding(menu(u'Pythons', choices), left=2, right=2)
+top = urwid.Overlay(main, urwid.SolidFill(u'\N{MEDIUM SHADE}'),
+    align='center', width=('relative', 60),
+    valign='middle', height=('relative', 60),
+    min_width=20, min_height=9)
+urwid_loop = urwid.MainLoop(top,palette=[('reversed','standout','')],event_loop=urwid.AsyncioEventLoop(loop=asyncio.get_event_loop()))
+urwid_loop.run()
